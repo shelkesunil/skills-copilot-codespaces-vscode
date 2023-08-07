ad5cb5ebe8aff921289cd4fc7dc439057710906a
Sunil <140670398+shelkesunil@users.noreply.github.com>
2023-08-07 09:12:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// create web server
+
+var app = http.createServer(function (request, response) {
+    // 2.1. when user requests something from the server
+    // 2.1.1. get query data from the URL
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    // 2.1.2. get pathname from the URL 
+    var pathname = url.parse(_url, true).pathname;
+    // 2.1.3. if user requests the root path
+    if (pathname === '/') {
+        //
+
