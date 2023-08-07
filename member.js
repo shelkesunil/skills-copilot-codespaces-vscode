71d4f30f4528f9bb0ac080a3cbe469efe7ec2259
Sunil <140670398+shelkesunil@users.noreply.github.com>
2023-08-07 08:48:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillmember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var memberlist = document.getElementById("memberlist");
+    var skilllist = document.getElementById("skilllist");
+    var projectlist = document.getElementById("projectlist");
+    member.style.color = "#fff";
+    skill.style.color = "#000";
+    project.style.color = "#000";
+    memberlist.style.display = "block";
+    skilllist.style.display = "none";
+    projectlist.style.display = "none";
+}
